4bf187050b5a93b41b6d8abcb3c2df85e7c82f2a
Pury-01 <puryokumu@gmail.com>
2024-12-05 13:22:23 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ A module for a simple Cache class using Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,7 +25,8 @@ class Cache:
          Store data in Redis with a randomly generated key.
 
         Args:
-            data (Union[str, bytes, int, float]): The data to store in Redis.
+            data (Union[str, bytes, int, float]): The data
+            to store in Redis.
 
         Returns:
             str: The randomly generated key.
@@ -33,3 +34,56 @@ class Cache:
         key = str(uuid.uuid4())  # Generate a random UUID as the key
         self._redis.set(key, data)  # Store the data in Redis
         return key
+
+    def get(
+            self, key: str,
+            fn: Optional[
+                Callable[
+                    [bytes],
+                    Union[str, int, bytes, float]
+                    ]
+                ] = None
+    ) -> Union[str, int, bytes, None]:
+        """
+        Retrieve data from Redis by key and optionally
+        transform it using fn.
+
+        Args:
+            key (str): The key to retrieve the data.
+            fn (Optional[Callable]): A callable function
+            to transform the data.
+
+        Returns:
+            Union[str, int, bytes, None]: The retrieved data
+            transformed if fn is provided.
+        """
+        value = self._redis.get(key)  # Retrieve the value from Redis
+        if value is None:  # Key does not exist
+            return None
+        return fn(value) if fn else value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string value from Redis by key.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[str]: The retrieved string or None
+            if the key doesn't exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer value from Redis by key.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[int]: The retrieved integer or
+            None if the key doesn't exist.
+        """
+        return self.get(key, fn=int)
